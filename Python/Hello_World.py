3fe603bb3038908a3a2f76a67dfcca953065ddf7
Manoj Shetty <manojtshetty@gmail.com>
2020-07-01 22:31:00 -0400

    new commit

--- Python/Hello_World.py
+++ Python/Hello_World.py
@@ -1,5 +1,12 @@
 def add (w,f):
     return w+f
 
+def mul(x,y):
+    return x*y
+
+def greet(fname,lname):
+    print('Welcome to the world of python ...'+fname+' '+lname)
 if __name__ == "__main__":
     print( add(5,4))
+    print( mul(5,4))
+    greet('Manoj','Shetty')
